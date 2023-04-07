0636caba153318259b7b685bd06ff00f6c759ee6
Robert Smith <101079914+brprod8@users.noreply.github.com>
2023-04-07 11:13:32 -0700

    Add files via upload

--- Play Sound/Sound.js
+++ Play Sound/Sound.js
@@ -1,69 +1,120 @@
-function getAudio(audioSrc) {
-  return new Audio(audioSrc);
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+if (!window.AudioContext) {
+  console.error("AudioContext not supported!");
 }
-
+// Define an object containing audio track data
+// Define an object containing audio track data
 const audioTracks = [
-  {
-    name: 'UAT Space Program',
-    src: 'resource/us-lab-background.mp3'
-  },
-  {
-    name: 'Alien',
-    src: 'resource/mixkit-alien-jungle-world-2417.wav'
-  },
-  {
-    name: 'Space Junk',
-    src: 'resource/mixkit-coarse-tone-mic-distortion-2130.wav'
-  }
+  { name: 'UAT Space Program', src: 'resource/us-lab-background.mp3' },
+  { name: 'Alien', src: 'resource/mixkit-alien-jungle-world-2417.wav' },
+  { name: 'Space Junk', src: 'resource/mixkit-coarse-tone-mic-distortion-2130.wav' },
 ];
 
+// Define global variables
+let audio;
 let currentTrackIndex = 0;
-let audio = getAudio(audioTracks[currentTrackIndex].src);
+let loop = false;
 
-const playButton = document.getElementById('play-button');
-const playButton2 = document.getElementById('play-button-2');
-const playButton3 = document.getElementById('play-button-3');
-const stopButton = document.getElementById('stop-button');
+// Get references to DOM elements
 const trackName = document.getElementById('audio-name');
+const playButton = document.querySelector('[data-track-index="0"]');
+const playButton2 = document.querySelector('[data-track-index="1"]');
+const playButton3 = document.querySelector('[data-track-index="2"]');
+const volumeSlider = document.getElementById('volume-slider');
+const muteButton = document.getElementById('mute-button');
+const loopButton = document.getElementById('loop-button');
 
-playButton.addEventListener('click', () => {
-  currentTrackIndex = 0;
+// Define function to play audio
+function playAudio(trackIndex) {
+  if (audio) {
+    audio.pause();
+  }
+
+  currentTrackIndex = trackIndex;
   audio = getAudio(audioTracks[currentTrackIndex].src);
-  audio.currentTime = 0;
-  audio.play();
-  trackName.innerText = audioTracks[currentTrackIndex].name;
+
+  if (audio) {
+    audio.currentTime = 0;
+    audio.play();
+    trackName.innerText = audioTracks[currentTrackIndex].name;
+  }
+}
+
+// Define function to stop audio
+function stopAudio() {
+  if (audio) {
+    audio.pause();
+    audio.currentTime = 0;
+    trackName.innerText = '';
+  }
+}
+
+// Add click event listeners to the play buttons
+playButton.addEventListener('click', () => {
+  playAudio(0);
 });
 
 playButton2.addEventListener('click', () => {
-  currentTrackIndex = 1;
-  audio = getAudio(audioTracks[currentTrackIndex].src);
-  audio.currentTime = 0;
-  audio.play();
-  trackName.innerText = audioTracks[currentTrackIndex].name;
+  playAudio(1);
 });
 
 playButton3.addEventListener('click', () => {
-  currentTrackIndex = 2;
-  audio = getAudio(audioTracks[currentTrackIndex].src);
-  audio.currentTime = 0;
-  audio.play();
-  trackName.innerText = audioTracks[currentTrackIndex].name;
+  playAudio(2);
 });
 
-stopButton.addEventListener('click', () => {
-  audio.pause();
+// Add input event listener to the volume slider
+volumeSlider.addEventListener('input', () => {
+  if (audio) {
+    audio.volume = volumeSlider.value / 100;
+  }
 });
 
-const stopAllSoundsButton = document.getElementById('stop-all-sounds-button');
-stopAllSoundsButton.addEventListener('click', () => {
-  audio.pause();
-  currentTrackIndex = 0;
-  audio = getAudio(audioTracks[currentTrackIndex].src);
-  trackName.innerText = "";
-});
-
-buttonArray = [playButton, playButton2, playButton3, stopButton];
-
-for (let i = 0; i < buttonArray.length; i++) {
-  buttonArray[i].classList.add('button');
+// Get an audio element with a given source
+function getAudio(src) {
+  const audio = new Audio(src);
+  audio.volume = volumeSlider.value / 100;
+  audio.loop = loop;
+  return audio;
 }
+
+// Set the initial volume to 50%
+let volume = 0.5;
+
+// Add event listener to volume slider
+volumeSlider.addEventListener('input', () => {
+  volume = volumeSlider.value / 100;
+  setVolume(volume);
+});
+
+// Function to set the volume of the audio element
+function setVolume(volume) {
+  if (audio) {
+    audio.volume = volume;
+  }
+}
+
+// Add click event listener to mute button
+muteButton.addEventListener('click', () => {
+  if (audio) {
+    audio.muted = !audio.muted;
+    if (audio.muted) {
+      muteButton.innerText = 'Unmute';
+    } else {
+      muteButton.innerText = 'Mute';
+    }
+  }
+});
+
+// Add click event listener to loop button
+loopButton.addEventListener('click', () => {
+  if (audio) {
+    loop = !loop;
+    audio.loop = loop;
+    if (loop) {
+      loopButton.innerText = 'Unloop';
+    } else {
+      loopButton.innerText = 'Loop';
+    }
+  }
+});
+
